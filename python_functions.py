60ff67f92ee213e65b137b56b3e44e56b7cdb58d
Munira Mohamed <muniramohamed@Muniras-MBP.broadband>
2021-05-20 15:49:09 +0100

    functions

---
+++ python_functions.py
@@ -0,0 +1,20 @@
+# Let's create a basic functions to greet
+# Syntax def name():
+
+# first iterations
+# def greetings():
+#     return "Welcome to Cyber Security! " #
+
+# print(greetings())
+
+# Second iteration
+def greeting_user(name):
+    return "welcome on board1"
+
+def greeting(name):
+    print(f'Hello {name}')
+    print('Welcome to Cyber Security')
+
+user = input('What is your name? ')
+greeting(user)
+# Take user name as input() and disply is back to them with greeting messages of your choice
