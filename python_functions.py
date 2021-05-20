5014c56e1baff459761ca2a1195d866e971457aa
Munira Mohamed <muniramohamed@Muniras-MBP.broadband>
2021-05-20 16:03:23 +0100

    functions added with args

--- python_functions.py
+++ python_functions.py
@@ -7,14 +7,28 @@
 
 # print(greetings())
 
-# Second iteration
-def greeting_user(name):
-    return "welcome on board1"
+# # Second iteration
+# def greeting_user(name):
+#     return "welcome on board1"
+#
+# # Take user name as input() and disply is back to them with greeting messages of your choice
+#
+# def greeting(name):
+#     print(f'Hello {name}')
+#     print('Welcome to Cyber Security')
+#
+# user = input('What is your name? ')
+# greeting(user)
 
-def greeting(name):
-    print(f'Hello {name}')
-    print('Welcome to Cyber Security')
 
-user = input('What is your name? ')
-greeting(user)
-# Take user name as input() and disply is back to them with greeting messages of your choice
+ # creating a function that takes 2 args as ints
+def add(value1, value2):
+    return value1 + value2
+print(add(2,3))
+
+def subtract(value1, value2):
+    return value1 - value2
+print(subtract(9, 5))
+
+
+
